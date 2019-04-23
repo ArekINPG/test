21eabc905cc10119c4ca5a5886641976c327c764
Arek <49916799+ArekINPG@users.noreply.github.com>
2019-04-23 18:34:59 +0200

    Create test.c

---
+++ test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define R 5
+
+double** createMatrix(int n)
+{
+    int * tab=malloc(n*sizeof(n*sizeof(double)));
+    return &tab;
+}
+void fillMatrix(double** tab, int n)
+{
+    int x=1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            tab[i][j]=x;
+            x++;
+        }
+    }
+}
+void printMatrix(double** tab, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%4lf ",tab[i][j])
+        }
+    printf("\n");
+    }
+}
+void deleteMatrix(double** tab, int n)
+{
+    ;
+}
+
+int main()
+{
+    double** tab=createMatrix(R);
+    fillMatrix(tab, R);
+    printMatrix(tab, R);
+    free(tab);
+    return 0;
+
+}
